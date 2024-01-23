29a002e3e1c4e86618ffaf17980caed4cf71a928
Tnalxmsk <alstnwkd990@naver.com>
2024-01-23 22:20:19 +0900

    feat: 재시작 입력 기능 추가

--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -9,7 +9,14 @@ class InputView {
         return input.map { it.digitToInt() }
     }
 
+    fun readRestart(): Int {
+        println(READ_RESTART_MESSAGE)
+        val input = Console.readLine()
+        return input.toInt()
+    }
+
     companion object {
         private const val READ_NUMBER_MESSAGE = "숫자를 입력해주세요 : "
+        private const val READ_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
     }
 }
