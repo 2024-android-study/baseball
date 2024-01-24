5b8365cd369684610e7402b5999197afd33787cd
gretea5 <gretea4489@gmail.com>
2024-01-24 20:36:21 +0900

    refactor: InputChecker 클래스 생성 및 입력 예외 처리 메서드 이동

---
+++ src/main/kotlin/baseball/checker/InputChecker.kt
@@ -0,0 +1,38 @@
+package baseball.checker
+
+import baseball.enums.BaseballNumber
+
+class InputChecker {
+    //야구 숫자 입력 체크
+    fun numbersCheck(numbers: String) {
+        isNumber(numbers)
+        isValidLength(numbers)
+        hasDuplicateNumbers(numbers)
+    }
+
+    //재시작 종료 여부 체크
+    fun restartInputCheck(restartInput: String) {
+        isNumber(restartInput)
+        isCorrectRestartInput(restartInput)
+    }
+
+    private fun isValidLength(numbers: String) {
+        if (numbers.length != BaseballNumber.REQUIRED_NUMBER_SIZE.number)
+            throw IllegalArgumentException("세자리 숫자가 입력되지 않았습니다.")
+    }
+
+    private fun isNumber(numbers: String) {
+        for (number in numbers) {
+            if (!number.isDigit()) throw IllegalArgumentException("숫자가 아닌 문자를 입력하셨습니다.")
+        }
+    }
+
+    private fun hasDuplicateNumbers(numbers: String) {
+        if (numbers.length != numbers.toSet().size) throw IllegalArgumentException("중복된 숫자를 입력하셨습니다.")
+    }
+
+    private fun isCorrectRestartInput(restartInput: String) {
+        if (restartInput.toInt() != BaseballNumber.RESTART_INPUT.number && restartInput.toInt() != BaseballNumber.END_INPUT.number)
+            throw IllegalArgumentException("입력하신 숫자가 올바른 숫자가 아닙니다.")
+    }
+}
--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -1,14 +1,17 @@
 package baseball.view
 
+import baseball.checker.InputChecker
 import baseball.enums.BaseballNumber
 import camp.nextstep.edu.missionutils.Console
 
 class InputView {
+    private val inputChecker = InputChecker()
+
     fun readNumbers(): List<Int> {
         print("숫자를 입력해주세요 : ")
         val numbers = Console.readLine()
 
-        numbersCheck(numbers)
+        inputChecker.numbersCheck(numbers)
 
         return numbers.map { it.toString().toInt() }
     }
@@ -17,39 +20,8 @@ class InputView {
         println("게임을 새로 시작하려면 ${BaseballNumber.RESTART_INPUT.number}, 종료하려면 ${BaseballNumber.END_INPUT.number}를 입력하세요.")
         val restartInput = Console.readLine()
 
-        restartInputCheck(restartInput)
+        inputChecker.restartInputCheck(restartInput)
 
         return restartInput.toInt()
-    }
-
-    fun numbersCheck(numbers: String) {
-        isNumber(numbers)
-        isValidLength(numbers)
-        hasDuplicateNumbers(numbers)
-    }
-
-    fun restartInputCheck(restartInput: String) {
-        isNumber(restartInput)
-        isCorrectRestartInput(restartInput)
-    }
-
-    fun isValidLength(numbers: String) {
-        if (numbers.length != BaseballNumber.REQUIRED_NUMBER_SIZE.number)
-            throw IllegalArgumentException("세자리 숫자가 입력되지 않았습니다.")
-    }
-
-    fun isNumber(numbers: String) {
-        for (number in numbers) {
-            if (!number.isDigit()) throw IllegalArgumentException("숫자가 아닌 문자를 입력하셨습니다.")
-        }
-    }
-
-    fun hasDuplicateNumbers(numbers: String) {
-        if (numbers.length != numbers.toSet().size) throw IllegalArgumentException("중복된 숫자를 입력하셨습니다.")
-    }
-
-    fun isCorrectRestartInput(restartInput: String) {
-        if (restartInput.toInt() != BaseballNumber.RESTART_INPUT.number && restartInput.toInt() != BaseballNumber.END_INPUT.number)
-            throw IllegalArgumentException("입력하신 숫자가 올바른 숫자가 아닙니다.")
     }
 }
