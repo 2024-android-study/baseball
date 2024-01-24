d1171f0e1c5bfa771f88572e670ee68c470a546b
권민채 <minchae075@naver.com>
2024-01-24 20:06:55 +0900

    feat: 컴퓨터 랜덤 숫자 3개 생성

---
+++ src/main/kotlin/baseball/Computer.kt
@@ -0,0 +1,20 @@
+package baseball
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class Computer {
+    // 컴퓨터 난수 생성
+    fun createNum() {
+        val computer = mutableListOf<Int>()
+
+        while (computer.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber)
+            }
+        }
+
+//        println(computer)
+    }
+}
