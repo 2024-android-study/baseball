7ca1293e0cef374ed02be1ab0192e3e2f00f7f37
Dongwon Shin <edvedv@naver.com>
2024-01-25 21:37:23 +0900

    feat: 입력 및 입력 유효성 검사 기능 추가, 상수 추가

---
+++ src/main/kotlin/baseball/Constant.kt
@@ -0,0 +1,14 @@
+package baseball
+
+class Constant {
+    companion object {
+        const val START = "숫자 야구 게임을 시작합니다."
+        const val INPUT = "숫자를 입력해주세요 : "
+        const val CORRECT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+        const val RESTART_OR_END = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: "
+
+        const val NOTHING = "낫싱"
+        const val STRIKE = "스트라이크"
+        const val BALL = "볼"
+    }
+}
---
+++ src/main/kotlin/baseball/Input.kt
@@ -0,0 +1,26 @@
+package baseball
+
+import baseball.Constant.Companion.INPUT
+import camp.nextstep.edu.missionutils.Console
+class Input {
+    fun getInput(): List<Int> {
+        print(INPUT)
+        val input = Console.readLine()
+        checkInput(input)
+        return input.map { it.toString().toInt() }
+    }
+
+    private fun checkInput(input: String) : Boolean{
+        if(input.length != 3) throw IllegalArgumentException("세 자리 숫자를 입력해주세요.")
+
+        for(i in input) {
+            if(!i.isDigit()) throw IllegalArgumentException("숫자를 입력해주세요.")
+        }
+
+        if(input.toSet().size != input.length) throw IllegalArgumentException("서로 다른 숫자를 입력해주세요.")
+
+        if(input.all { it in '1'..'9' }) throw IllegalArgumentException("1~9 범위의 숫자를 입력해주세요.")
+
+        return true
+    }
+}
